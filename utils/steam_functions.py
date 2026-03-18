b8ce0f99ce4193f67d7e7c2b2484c09743ef1e63
bcvilnrotter <35104906+bcvilnrotter@users.noreply.github.com>
2026-03-18 19:47:44 -0400

    Fix page name formatting in Notion data function

--- utils/steam_functions.py
+++ utils/steam_functions.py
@@ -41,8 +41,9 @@ def format_2week_playtime_to_notion_data(raw_playtime_dbid,game_data):
     today = datetime.utcnow().strftime('%Y-%m-%d')
     two_weeks_ago = (datetime.utcnow() - timedelta(days=14)).strftime('%Y-%m-%d')
     if game_data['name']:
+        page_name = f'{game_data['name']}_{today}'
         properties = {
-            "Name": {"title": [{"text": {"content": f'{game_data['']}_{today}'}}]},
+            "Name": {"title": [{"text": {"content": page_name}}]},
             "AppId": {"rich_text": [{"text": {"content": str(game_data['appid'])}}]},
             "Date Range": {"date": {"start": two_weeks_ago, "end":today}},
             "playtime_2weeks": {"number": game_data['playtime_2weeks']},
