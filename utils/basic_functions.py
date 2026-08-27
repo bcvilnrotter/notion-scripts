bb2bf43eb5bf308f4ead015f32e8f54b5ea81d5a
bcvilnrotter <bcvilnrotter@gmail.com>
2026-08-26 22:51:32 -0500

    basic dryrun created, python 3.11 works max

--- utils/basic_functions.py
+++ utils/basic_functions.py
@@ -9,8 +9,8 @@ from pandas.api.types import *
 def get_secret(secret_key):
     if not os.getenv(secret_key):
         print(f"Loading .env for {secret_key}")
-        #env_path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..\..','.gitignore\.env'))
-        env_path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..\..\..','.gitignore\.env'))
+        env_path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','..','.gitignore','.env'))
+        #env_path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','..','..','.gitignore','.env'))
         load_dotenv(dotenv_path=env_path)
 
     value = os.getenv(secret_key)
