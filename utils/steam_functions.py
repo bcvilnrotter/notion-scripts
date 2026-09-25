a4294a9212092c1a3029d063855bf7ee359d00fb
bcvilnrotter <35104906+bcvilnrotter@users.noreply.github.com>
2026-09-25 01:06:02 -0500

    Add sleep delay in get_banner_url_from_appid function
    Added a sleep delay before fetching the banner URL.

--- utils/steam_functions.py
+++ utils/steam_functions.py
@@ -22,6 +22,7 @@ def get_all_page_atts(headers,database_id):
         return {}
 
 def get_banner_url_from_appid(appid):
+    sleep(5)
     game_url = f'https://store.steampowered.com/api/appdetails?appids={appid}'
     response = requests.get(game_url,stream=True)
     if check_video_game_page_exists(response,appid):
